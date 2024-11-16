aef90425d0d0b6c3d1695e25738e1fd6877d8082
Julia <s202359@student.pg.edu.pl>
2024-11-16 16:02:41 +0100

    wagi niedokonczone

---
+++ wagi-ss.R
@@ -0,0 +1,27 @@
+
+
+w1 <- c(seq(from = -1, to =1, by = 0.01 ))
+w2 <- c(seq(from = -1, to =1, by = 0.01 ))
+w3 <- c(seq(from = -1, to =1, by = 0.01 ))
+w4 <- c(seq(from = -1, to =1, by = 0.01 ))
+wagi <- data.frame(w1, w2,w3,w4)
+
+w1+w2+w3+w4==1
+
+# for(wagi$w1) elementy z wektora 1
+#   for(wagi$w2) elementy z wektora 2
+#     for(wagi$w3) elementy z wektora 3
+#       for(wagi$w4) elementy z wektora 4
+#       if w1+w2+w3+w4==1 suma jest 1 to dodaj kombinację do tabelki
+
+
+
+
+
+
+
+
+
+
+
+
